5867fb4ae6ba37ae210f21f0d204c0e2cc12073c
CubicActivity <stojmanovski355@gmail.com>
2026-04-18 17:09:59 +0200

    Simple modle

---
+++ InterLinked/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using InterLinked.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace InterLinked.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<InterlinkedAppUser>(options)
+    {
+    }
+}
